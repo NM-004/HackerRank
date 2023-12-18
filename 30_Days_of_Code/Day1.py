ac673735684ef6b06129c05e37bcd2406b992f20
NM003 <103164984+NM003@users.noreply.github.com>
2023-12-18 21:28:56 +0530

    Adding 30 Days of Code

---
+++ 30_Days_of_Code/Day1.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+a=input()
+b=input()
+c=input()
+e=int(i)+int(a)
+print(e)
+f=float(d)+float(b)
+print(f)
+g=str(s)+str(c)
+print(g)
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
